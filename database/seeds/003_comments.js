a2354e2259ebd1648bfce6b9e58df6743eef55a9
Jengopockets <jengopockets@gmail.com>
2020-02-25 19:04:08 -0500

    Set up seeds

--- database/seeds/003_comments.js
+++ database/seeds/003_comments.js
@@ -5,10 +5,10 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('comments').insert([
-        {comment: 'A Default Comment', bleep_id:'1'},
-        {comment: 'A Default Comment', bleep_id:'4'},
-        {comment: 'A Default Comment', bleep_id:'3'},
-        {comment: 'A Default Comment', bleep_id:'1'},
+        {comment: 'Wow you are dense huh', bleep_id:'1'},
+        {comment: 'Yes Yes Yes', bleep_id:'4'},
+        {comment: 'Do you belong on the internet?', bleep_id:'3'},
+        {comment: 'Go home you\'re drunk', bleep_id:'1'},
       ]);
     });
 };
