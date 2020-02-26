8b2fdd6b15e00578a0613b561a87a256d1ad0bd7
Jengopockets <jengopockets@gmail.com>
2020-02-25 22:01:43 -0500

    Grabs user

--- api/sever.js
+++ api/sever.js
@@ -2,13 +2,18 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 
+const authRouter = require('../auth/authRouter');
+
 const server = express()
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
+server.use('/api/auth', authRouter);
+
 server.get('/', (req, res) => {
     res.send('Its alive :smile:')
 })
+
 
 module.exports = server;
